0e90b59bd4d4f4ddae7770be9a8bee3ae8de80af
NIKHIL TOM <118368860+nikhiltom369@users.noreply.github.com>
2025-06-17 18:36:51 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+// Use the environment variable or fallback to the Railway URL
+const API_URL = process.env.REACT_APP_API_URL || 'https://backendnews-production-0bc6.up.railway.app';
 
 export const api = {
   getCategories: () => axios.get(`${API_URL}/categories`),
